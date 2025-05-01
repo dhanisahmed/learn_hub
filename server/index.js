b76af1e121681f7cedcb83007329706be6c400a5
venkey45R <venkateshamulraj@gmail.com>
2025-05-01 18:45:11 +0530

    Added port

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
 mongoose.connect("mongodb://localhost:27017/yourDBName", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
